221aea0caa3540fc6763036de69355f91b0bf922
Bob Tipton <btipton@darkskyinnovation.com>
2020-07-06 11:33:38 +0800

    Working cron script

--- pull_who.sh
+++ pull_who.sh
@@ -1,1 +1,2 @@
-wget -P /public_html/covid_tracker/ https://covid19.who.int/WHO-COVID-19-global-data.csv
+wget https://covid19.who.int/WHO-COVID-19-global-data.csv
+mv WHO-COVID-19-global-data.csv public_html/covid_tracker
